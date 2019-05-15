954400eb889d336612e957a4a2fba7360c0b9c2b
“othaderek” <otha.hernandez@gmail.com>
2019-05-15 08:36:12 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
-## code your solution here. 
+## code your solution here.
+class Cat
+  attr_accessor :name, :meow
+
+  def meow(input="meow!")
+    @meow = input
+    puts @meow
+
+  end
+end
+
+maru = Cat.new
+
+puts maru.meow
